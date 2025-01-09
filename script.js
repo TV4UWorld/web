5e625bcc120025e2ba41de2ba512aa0a8ed24f27
TV4UWorld <88163709+TV4UWorld@users.noreply.github.com>
2025-01-09 17:15:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Definicije odgovora za pitanja
 const answers = {
-    "Pitanje 1: Što je TV4U?": "TV4U je usluga koja omogućuje korisnicima gledanje omiljenih kanala putem interneta.",
-    "Pitanje 2: Kako se prijaviti?": "Registracija je jednostavna putem naše web stranice u nekoliko koraka.",
-    "Pitanje 3: Koliko košta usluga?": "Cijena usluge ovisi o paketu, a informacije možete pronaći na stranici s cijenama.",
+    "Pitanje 1: GLOBAL TELECOM OTT OTV Aplikacija": "GLOBAL TELECOM OTT OTV aplikacija omogućuje korisnicima pristup velikom broju TV kanala i sadržaja putem interneta. Više informacija o aplikaciji možete pronaći na službenoj stranici.",
+    
+    "Pitanje 2: Instalacija i registracija na ProtonVPN.": `
+        <strong>Android:</strong> Većina pametnih telefona i tableta koristi Android. Proton VPN aplikaciju možete besplatno preuzeti <a href="#" target="_blank">ovdje</a>.<br><br>
+        <strong>iOS:</strong> Ako imate iPhone ili iPad, Proton VPN možete besplatno preuzeti <a href="#" target="_blank">ovdje</a>.<br><br>
+        <strong>Windows:</strong> Proton VPN je dostupan za besplatno preuzimanje za Windows uređaje <a href="#" target="_blank">ovdje</a>.<br><br>
+        <strong>macOS:</strong> Za korisnike Apple Mac računala, Proton VPN možete instalirati <a href="#" target="_blank">ovdje</a>.<br><br>
+        <strong>Linux:</strong> Linux korisnici mogu preuzeti Proton VPN za svoju distribuciju <a href="#" target="_blank">ovdje</a>.<br><br>
+        <strong>Proširenje za preglednik:</strong> Dostupno za korisnike s plaćenim Proton VPN planom na <a href="#" target="_blank">Chrome</a> i <a href="#" target="_blank">Firefox</a>.
+    `,
+
+    "Pitanje 3: Cjenik usluge i plaćanje.": "Cijena usluge ovisi o odabranom paketu. Plaćanje je moguće izvršiti putem kreditnih kartica, PayPala ili drugih dostupnih metoda. Detalje o cijenama i paketima možete pronaći na našoj stranici Cjenik."
 };
 
 // Kada se klikne na pitanje, prikaži odgovor desno
