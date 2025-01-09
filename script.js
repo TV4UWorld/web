401705bd77c8d447ca0514784ef6f6728896d0ce
TV4UWorld <88163709+TV4UWorld@users.noreply.github.com>
2025-01-09 16:43:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ document.querySelectorAll('.accordion').forEach((button) => {
         }
     });
 });
+
+// Postavi status usluge
+const serviceStatus = document.getElementById('service-status');
+const statusLight = document.getElementById('status-light');
+
+function setStatus(isActive, reason) {
+    if (isActive) {
+        serviceStatus.textContent = 'AKTIVNA';
+        statusLight.classList.remove('inactive');
+        statusLight.classList.add('active');
+    } else {
+        serviceStatus.textContent = 'NEAKTIVNA';
+        statusLight.classList.remove('active');
+        statusLight.classList.add('inactive');
+    }
+    if (reason) {
+        document.getElementById('reason').textContent = `Razlog: ${reason}`;
+    }
+}
+
+// Primjer promjene statusa
+// setStatus(false, 'Privremeni problemi s uslugom.');
