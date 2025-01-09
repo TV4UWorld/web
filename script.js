49cd7dfc2f0ae25990fc66dad5334f817debb82f
TV4UWorld <88163709+TV4UWorld@users.noreply.github.com>
2025-01-09 21:30:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.querySelectorAll('.accordion').forEach((button) => {
     });
 });
 
-document.getElementById('service-status').textContent = 'NEAKTIVNA';
-document.getElementById('status-light').className = 'status-light inactive';
-document.getElementById('reason').textContent = 'Razlog: Planirani radovi.';
+document.getElementById('service-status').textContent = 'AKTIVNA';
+document.getElementById('status-light').className = 'status-light active';
+document.getElementById('reason').textContent = 'Sve usluge su trenutno dostupne.';
 
